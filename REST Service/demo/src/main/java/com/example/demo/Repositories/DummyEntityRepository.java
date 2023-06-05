7b3dba103123d7a81943fc14266d4af4ea0970b6
Thorvas <cygan.kacper@outlook.com>
2023-06-05 15:55:08 +0200

    Comments added, SpecificationBuilder class created, Mapper class created

--- REST Service/demo/src/main/java/com/example/demo/Repositories/DummyEntityRepository.java
+++ REST Service/demo/src/main/java/com/example/demo/Repositories/DummyEntityRepository.java
@@ -4,13 +4,13 @@ package com.example.demo.Repositories;
 import com.example.demo.DummyObject.DummyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
 
+/**
+ * Repository for DummyEntity queries
+ */
 @Repository
 public interface DummyEntityRepository extends JpaRepository<DummyEntity, Long>, JpaSpecificationExecutor<DummyEntity> {
+
 }
