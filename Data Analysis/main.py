d5c3bbd638358f5a1d8233473aaa02d08339ac6b
micnow815 <micnow815@gmail.com>
2023-06-09 17:44:23 +0200

    Removed powiat warszawski
    Removed non-existent powiat

--- Data Analysis/main.py
+++ Data Analysis/main.py
@@ -8,7 +8,7 @@ powiat = []
 
 # Because of API's system, I need to reconstruct a name od a powiat and exclude another one
 for i in range(get_total_powiats()):
-    if get_powiat_name(i) != 'Powiat m. Wałbrzych do 2002':
+    if get_powiat_name(i) != 'Powiat m. Wałbrzych do 2002' and get_powiat_name(i) != "Powiat warszawski":
         if get_powiat_name(i) != 'Powiat m. Wałbrzych od 2013':
             item = Powiat(get_powiat_name(i), get_powiat_population(i), 0, get_current_date())
         else:
